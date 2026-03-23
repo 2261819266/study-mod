3e44fe7d6e073654cec36b37b512c4870b92f83d
Vastosine <2261819266@qq.com>
2026-03-23 09:36:47 +0800

    origin item

---
+++ src/main/java/com/vas/study/StudyItem.java
@@ -0,0 +1,27 @@
+package com.vas.study;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+import static com.vas.study.Study.MOD_ID;
+
+public class StudyItem {
+
+    public static final Item STUDY_ITEM = registerModItem("study_item");
+
+    private static Item registerModItem(final String name) {
+        final ResourceKey<Item> key = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, name));
+        Item item = (Item) ((Function<Item.Properties, Item>) (Item::new)).
+                apply(new Item.Properties().setId(key));
+        return Registry.register(BuiltInRegistries.ITEM, key, item);
+    }
+
+    public static void initialize() {
+    }
+}
